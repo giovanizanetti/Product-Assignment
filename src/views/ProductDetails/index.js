67087278370bb57a2334ac36e231e9e56ad2352c
giovanizanetti <zanetti.giovani@gmail.com>
2021-11-18 12:30:25 +0100

    style
    create product detail component grid

--- src/views/ProductDetails/index.js
+++ src/views/ProductDetails/index.js
@@ -1,11 +1,37 @@
 import { useParams } from 'react-router-dom'
 import { useProduct } from '../../hooks/useProduct'
+import { useEffect } from 'react'
 
 const Product = () => {
   const { product } = useParams()
   const currentProduct = useProduct(product)
   const { titlePlural } = currentProduct
-  return <div data-testid='productDetails-component'>{titlePlural}</div>
+
+  useEffect(() => {
+    const Properties = currentProduct.properties.map((property) => property.slug)
+    console.log(Properties)
+  }, [currentProduct])
+
+  const displayProductProperites = () => {
+    const productProperties = currentProduct.properties.map((property) => {
+      return <div key={property.slug}>{property.slug + ': ' + property.title}</div>
+    })
+    return productProperties
+  }
+
+  return (
+    <div className='container m-auto'>
+      <h1 className='p-5'>{titlePlural}</h1>
+      <div
+        data-testid='productDetails-component'
+        className='grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 '
+      >
+        {displayProductProperites()}
+      </div>
+    </div>
+  )
+
+  // <div >{titlePlural}</div>
 }
 
 export default Product
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
+
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+      },
+    },
   },
   variants: {
     extend: {},
